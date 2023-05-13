0656817642e72c1cfea6a4e808fd5a758244a73f
vhtrungd19369 <vhtrungd19369@cusc.ctu.edu.vn>
2023-05-13 15:38:52 +0700

    v3.1.0-d1305-FINAL-src-HTC: wp-content

--- wp-content/themes/flatsome/header.php
+++ wp-content/themes/flatsome/header.php
@@ -3,40 +3,17 @@
 <!--[if IE 8 ]> <html <?php language_attributes(); ?> class="ie8 <?php flatsome_html_classes(); ?>"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class="<?php flatsome_html_classes(); ?>"> <!--<![endif]-->
 <head>
-
-	<!-- Google Tag Manager -->
-	<!--<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':-->
-	<!--new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],-->
-	<!--j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=-->
-	<!--'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);-->
-	<!--})(window,document,'script','dataLayer','GTM-5VV5GFB');</script>-->
-	<!-- End Google Tag Manager -->
-
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<title>Cty TNHH TM XNK HATHACO</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900&amp;display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<?php wp_head(); ?>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-<!--<script async src="https://www.googletagmanager.com/gtag/js?id=G-YLBBM011CN"></script>-->
-<!--<script>-->
-<!--  window.dataLayer = window.dataLayer || [];-->
-<!--  function gtag(){dataLayer.push(arguments);}-->
-<!--  gtag('js', new Date());-->
-
-<!--  gtag('config', 'G-YLBBM011CN');-->
-<!--</script>-->
 </head>
-
-<body <?php body_class(); ?>>
-
-		<!-- Google Tag Manager (noscript) -->
-	<!--<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5VV5GFB"-->
-	<!--height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>-->
-	<!-- End Google Tag Manager (noscript) -->
 	
+<body <?php body_class(); ?>>	
 <?php do_action( 'flatsome_after_body_open' ); ?>
 <?php wp_body_open(); ?>
 
